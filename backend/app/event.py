355adef5a304dc83d2ebb578219956e10b311bcf
maxlearnscode <maxw.accs@gmail.com>
2023-04-06 14:30:33 +0200

    Every event now has its own site for event/ID

--- backend/app/event.py
+++ backend/app/event.py
@@ -1,6 +1,8 @@
 from typing import List
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from starlette.responses import HTMLResponse
+
 from . import schemas, crud, database
 
 router = APIRouter()
@@ -21,7 +23,20 @@ def read_event(event_id: int, db: Session = Depends(database.get_db)):
     db_event = crud.get_event(db=db, event_id=event_id)
     if db_event is None:
         raise HTTPException(status_code=404, detail="Event not found")
-    return db_event
+    html_content = f"""
+    <html>
+        <head>
+            <title>{db_event.name}</title>
+        </head>
+        <body>
+            <h1>{db_event.name}</h1>
+            <img src="{db_event.image_url}" alt="{db_event.name}">
+            <p>{db_event.address}</p>
+            <a href="{db_event.link}">{db_event.link}</a>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content)
 
 @router.put("/{event_id}", response_model=schemas.Event)
 def update_event(event_id: int, event: schemas.EventCreate, db: Session = Depends(database.get_db)):
